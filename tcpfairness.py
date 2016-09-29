3df863efd07c57f56adb3e344b77a9d33206067b
Lilika <lilika@mit.edu>
2016-09-29 16:31:42 +0000

    Working on 2

--- tcpfairness.py
+++ tcpfairness.py
@@ -14,6 +14,8 @@ from mininet.log import debug, error, setLogLevel
 from mininet.net import Mininet
 from mininet.nodelib import LinuxBridge
 from mininet.topo import Topo
+from time import sleep
+
 
 class CustomMininet(Mininet):
     """A Mininet with some custom features."""
@@ -94,8 +96,18 @@ class TwoSwitchTopo(Topo):
         h5 = self.addHost('h5')        
 
 
+       
+        self.addLink(h1, s1, cls1=BasicIntf, cls2=PIEIntf, bw=100, delay='10ms', max_queue_size=1000)
+        self.addLink(h2, s1, cls1=BasicIntf, cls2=PIEIntf, bw=100, delay='10ms', max_queue_size=1000)
+        self.addLink(h3, s1, cls1=BasicIntf, cls2=PIEIntf, bw=100, delay='10ms', max_queue_size=1000)
+
+        self.addLink(h4, s2, cls1=BasicIntf, cls2=PIEIntf, bw=100, delay='10ms', max_queue_size=1000)
+        self.addLink(h5, s2, cls1=BasicIntf, cls2=PIEIntf, bw=100, delay='10ms', max_queue_size=1000)
+
         # TODO: create link between switches, notice that the delay is different
-        	
+
+        self.addLink(s1, s2, cls1=PIEIntf, cls2=PIEIntf, bw=100, delay='30ms', max_queue_size=1000)
+	
 	# Hint: you can use **link_params to pass parameters when creating links
 	# Hint: remember to enable PIE for these links: for links between hosts and
 	# switches, mimic bufferbloat.py, PIE should sit on the switch side; for the
@@ -121,7 +133,7 @@ def main():
 
     # First, use CLI to explore this setting, i.e. run iperf on multiple hosts and run ping
     # Note: It can take a while to pop out xterm windows in GCP.
-    CLI(net)
+    #CLI(net)
     
     # TODO: Then, comment out CLI(net) and mimic bufferbloat.py, automate what you did in CLI, 
     # such that running aqm.py will output all the delivers in the problem.
@@ -132,9 +144,79 @@ def main():
     # Hint: "iperf -c ... -P 10 | grep SUM" gives aggregation information of 10 flows
     # "iperf -c -i 1 ..." gives a measurement every 1 second
     # Instead of tcpprobe, you can use "iperf -c ... > txtfile" to store the results.
-    # 
+    h1 = net.getNodeByName('h1')
+    h2 = net.getNodeByName('h2')
+    h3 = net.getNodeByName('h3')
+    h4 = net.getNodeByName('h4')
+    h5 = net.getNodeByName('h5')
+
+    
+    print "Starting h1 iperf server..."
+    server = h1.popen("iperf -s -w 16m")
+    print 'Open iperf connections h2->h1, and h5->h1'
+    h21 = h2.popen("iperf -c %s -t 500 -P 10 | grep SUM > T21-a.out" % (h1.IP()), shell = True)
+    h51 = h5.popen("iperf -c %s -t 500 -P 10 | grep SUM > T51-a.out" % (h1.IP()), shell = True)
+    sleep(100) 
+
+    print "Starting ping train h2->h1"
+    PT1 = h2.popen("ping -i 0.1 %s > RTT21-a.out" %( h1.IP()), shell = True)
+   
+    print "Starting ping train h5->h1"
+    PT2 = h5.popen("ping -i 0.1 %s > RTT15-a.out" %( h1.IP()), shell = True)
+    
+    print "Print about to sleep"
+    sleep(100)
+    print 'Trying to get RTT'
+    print getRTT('RTT21-a.out') 
+    print "Killing"
+    PT1.kill()
+    PT2.kill()
+    h21.kill()
+    h51.kill()
+
+ 
+    print "Starting h3 iperf server..."
+    server = h3.popen("iperf -s -w 16m") 
+   
+    print 'Open iperf connections h4->h3, h2->h1, and h5->h1'
+    h43 = h4.popen("iperf -c %s -t 500 -P 10 | grep SUM > T43-b.out" % (h3.IP()), shell = True)
+    h21 = h2.popen("iperf -c %s -t 500 -P 10 | grep SUM > T21-b.out" % (h1.IP()), shell = True)
+    h51 = h5.popen("iperf -c %s -t 500 -P 10 | grep SUM > T51-b.out" % (h1.IP()), shell = True)
+    sleep(100)
+    print "Starting ping trains h2->h1, h5->h1, h4->h3"
+    PT3 = h4.popen("ping -i 0.1 %s > RTT34-b.out" %( h3.IP()), shell = True)
+    PT2 = h5.popen("ping -i 0.1 %s > RTT15-b.out" %( h1.IP()), shell = True)
+    PT1 = h2.popen("ping -i 0.1 %s > RTT12-b.out" %( h1.IP()), shell = True)
+ 
+
+    print "Print about to sleep"
+    sleep(100)
+
+    
+
 
     net.stop()
+
+def getRTT(fileName):
+    f = open(fileName, 'r')
+    times = []
+
+    while True:
+        print 'k'
+        L =  f.readline()
+        if L == "":
+           break 
+        i = L.find("time=")
+        if i != -1:
+           z = L.find(" ms")
+           n = L[i+5:z]
+           times.append(float(n))
+    return numpy.average(times)
+
+
+
+
+
 
 if __name__ == '__main__':
     main()
